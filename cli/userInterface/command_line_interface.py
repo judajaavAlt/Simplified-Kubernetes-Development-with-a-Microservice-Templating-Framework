971de8d3aefc91664b44e8f7536b2307b491f319
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2026-03-23 16:59:01 -0500

    fix: remove unused functions

--- cli/userInterface/command_line_interface.py
+++ cli/userInterface/command_line_interface.py
@@ -1,14 +1,9 @@
 import sys
 
 
-def get_args() -> str:
+def get_args() -> list[str]:
     return sys.argv
 
 
 def output_message(message: str) -> None:
     print(message)
-
-
-def output_error(exception_type: Exception,
-                 exception_message: str) -> None:
-    raise exception_type(exception_message)
